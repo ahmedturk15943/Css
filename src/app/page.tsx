d0f87afd39d4ea750cfcc79c7fcf93b4baea4e19
ahmedturk15943 <ahmedturk748972@gmail.com>
2024-11-12 19:02:55 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />  
+       <Hero />
+      <About />
+       <Contact />
+       <Footer />
+    
+      
+    </div>
+  )
+}
+
+export default HomePage
+
+
